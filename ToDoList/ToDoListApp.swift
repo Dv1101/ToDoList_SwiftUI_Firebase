55a28647625b57db205b153c7b2e8ba2a166e540
Dv1101 <dhruvvaria1101@gmail.com>
2024-06-18 21:05:29 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dhruv Varia on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
